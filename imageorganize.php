6580e525213c9cd6190e8b22ce415d1365acb930
zhongjiwei <shoukii@kai.jp>
2015-05-28 15:23:05 +0900

    add result summary

--- imageorganize.php
+++ imageorganize.php
@@ -1,64 +1,93 @@
 <?php
-function find_all_files($dir) 
-{ 
-    $root = scandir($dir); 
-    foreach($root as $value) 
-    { 
-        if($value === '.' || $value === '..') {continue;} 
-        if(is_file("$dir/$value")) {$result[]="$dir/$value";continue;} 
-        foreach(find_all_files("$dir/$value") as $value) 
-        { 
-            $result[]=$value; 
-        } 
-    } 
-    return $result; 
-} 
 
+class ImageOrganize {
 
-#
-$folder = "E:/BaiduYunDownload";
-$output = "E:/img/";
-//var_dump($argv);
+    public $input;
+    public $output;
+    public $summary = array();
 
+    public function __construct($input, $output) {
+        $this->input = $input;
+        $this->output = $output;
+    }
 
-# all files 
-$files = find_all_files($folder);
+    private function find_all_files($dir) {
+        $root = scandir($dir);
+        foreach ($root as $value) {
+            if ($value === '.' || $value === '..') {
+                continue;
+            }
+            if (is_file("$dir/$value")) {
+                $result[] = "$dir/$value";
+                continue;
+            }
+            foreach (find_all_files("$dir/$value") as $value) {
+                $result[] = $value;
+            }
+        }
+        return $result;
+    }
 
-foreach( $files as $key => $file )
-{
-	if(file_exists($file)) {
-		
-		if (exif_imagetype($file) === IMAGETYPE_JPEG) {
+    private function organize_file($files, $output, &$summary) {
 
-			echo "=======================" . PHP_EOL;
-			echo "file : $file" . PHP_EOL;
-			
-			$exif = @exif_read_data($file);
+        foreach ($files as $key => $file) {
+            if (file_exists($file)) {
 
-			if(!isset($exif['DateTime'])) {
-				echo "[NG] | this images has not createtime information. " . PHP_EOL;
-				continue;
-			}
+                if (exif_imagetype($file) === IMAGETYPE_JPEG) {
 
-			// folder format : YYYY.MM.DD
-			$subfolder = substr($exif['DateTime'], 0, 10);
-			$subfolder = str_replace(":", ".", $subfolder);
+                    echo "=======================" . PHP_EOL;
+                    echo "file : $file" . PHP_EOL;
 
-			// if folder not exsit ,create folder 
-			if(!file_exists($output . $subfolder)) {
-				mkdir($output . $subfolder);
-			}
+                    $exif = @exif_read_data($file);
 
-			// copy files
-			$desc = $output . $subfolder . "/" . basename($file);
-			if(copy($file, $desc)){
-				echo "[OK] | $file => $desc" . PHP_EOL;
-			} else {
-				echo "[NG] | $file => $desc" . PHP_EOL;
-			}
+                    if (!isset($exif['DateTime'])) {
+                        echo "[NG] | this images has not createtime information. " . PHP_EOL;
+                        continue;
+                    }
 
-			echo PHP_EOL;
-		}
-	}
+                    // folder format : YYYY.MM.DD
+                    $subfolder = substr($exif['DateTime'], 0, 10);
+                    $subfolder = str_replace(":", ".", $subfolder);
+
+                    // if folder not exsit ,create folder 
+                    if (!file_exists($output . $subfolder)) {
+                        mkdir($output . $subfolder);
+                    }
+
+                    // copy files
+                    $desc = $output . $subfolder . "/" . basename($file);
+                    if (copy($file, $desc)) {
+                        echo "[OK] | $file => $desc" . PHP_EOL;
+                    } else {
+                        echo "[NG] | $file => $desc" . PHP_EOL;
+                    }
+
+                    // summary
+                    $summary[$subfolder][] = $desc;
+
+                    echo PHP_EOL;
+                }
+            }
+        }
+    }
+
+    private function show_summary($summary) {
+        foreach ($summary as $key => $value) {
+            echo "$key has " . count($value) . " pictures" . PHP_EOL;
+        }
+    }
+
+    public function run() {
+        $files = $this->find_all_files($this->input);
+        $this->organize_file($files, $this->output, $this->summary);
+        $this->show_summary($this->summary);
+    }
 
 }
+
+$input = "E:/BaiduYunDownload";
+$output = "E:/img/";
+
+$obj = new ImageOrganize($input, $output);
+$obj->run();
+
